11ce0f4b43ef262832357b96709c0652cb2a411d
divyanshugupta0 <150168350+divyanshugupta0@users.noreply.github.com>
2024-06-12 22:38:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,28 @@ $(document).ready(function(){
     });
 
 
+//  =======================================================mobile prohibit==========================================
 
+// Detect if the user is accessing from a mobile device
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Redirect if the user is accessing from a mobile device
+function redirectIfMobile() {
+    if (isMobile()) {
+        // Redirect to a different page
+        window.location.href = "https://example.com/not-allowed-on-mobile.html";
+    }
+}
+
+// Call the function to check and redirect on page load
+window.onload = redirectIfMobile;
+
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.href = "mobile_not_supported.html";
+}
 
 
 $('.navigation .menu li a').click(function(){
